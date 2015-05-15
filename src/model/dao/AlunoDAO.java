d4a931d25060c4cef42ecef96d416406d0e916f5
Fabiano Amaral Freitas <fabianin.amaral@gmail.com>
2015-05-15 15:21:43 -0300

    Criado AlunoDAO

---
+++ src/model/dao/AlunoDAO.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import java.util.ArrayList;
+import model.pojo.Aluno;
+
+/**
+ *
+ * @author Fabiano
+ */
+public class AlunoDAO {
+    ArrayList<Aluno> ListaAlunos = new ArrayList<>();
+    
+}
