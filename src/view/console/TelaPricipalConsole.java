33831d9d40bc7a4df6505ee6e16be4ad9474e4ad
Elias Júnior <eliasjnior@gmail.com>
2015-05-24 18:23:59 -0300

    Implementação do cadastroAluno na View

--- src/view/console/TelaPricipalConsole.java
+++ src/view/console/TelaPricipalConsole.java
@@ -5,6 +5,11 @@
  */
 package view.console;
 
+import exceptions.AlunoJaCadastradoException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.dao.AlunoDao;
 import model.dao.AtividadeDao;
 import model.dao.DisciplinaDao;
@@ -19,6 +24,7 @@ import model.dao.txt.FaltaDaoTxt;
 import model.dao.txt.NotaDaoTxt;
 import model.dao.txt.ProfessorDaoTxt;
 import model.dao.txt.TurmaDaoTxt;
+import model.pojo.Aluno;
 
 /**
  *
@@ -36,15 +42,30 @@ public class TelaPricipalConsole {
 
     public static void main(String[] args) {
         TelaPricipalConsole tpc = new TelaPricipalConsole();
-        tpc.showMenu();
+        int opcao;
+        do {
+            opcao = tpc.showMenu();
+            switch (opcao) {
+                case 1:
+                    tpc.cadastraAluno();
+                    break;
+                default:
+                    System.out.println("Nenhuma opção selecionada");
+                    System.out.println("");
+                    break;
+            }
+        } while (opcao != 0);
     }
 
-    private void showMenu() {
-        System.out.println("▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊");
-        System.out.println("▊▊▊▊▊▊▊▊ MENU PRINCIPAL ▊▊▊▊▊▊▊▊");
-        System.out.println("▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊▊");
+    private int showMenu() {
+
+        Scanner sc = new Scanner(System.in);
+        int answer = 0;
+
+        System.out.println("∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎");
+        System.out.println("∎∎∎∎∎∎∎∎∎∎∎∎∎ MENU PRINCIPAL ∎∎∎∎∎∎∎∎∎∎∎∎∎");
+        System.out.println("∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎");
         System.out.println("");
-        System.out.println("Escolha uma das opções: ");
         System.out.println("1) Cadastrar aluno");
         System.out.println("2) Cadastrar professor");
         System.out.println("3) Cadastrar disciplina");
@@ -56,6 +77,51 @@ public class TelaPricipalConsole {
         System.out.println("9) Consultar situação de aluno em uma disciplina");
         System.out.println("10) Consu11ltar o número de turmas já oferecidas por disciplina");
         System.out.println("11) Consultar o número de disciplinas já lecionadas por um professor");
+        System.out.println("");
+        System.out.print("Escolha uma das opções: ");
+
+        answer = sc.nextInt();
+
+        System.out.println("");
+
+        return answer;
+
+    }
+
+    private void cadastraAluno() {
+
+        Scanner sc = new Scanner(System.in);
+        String nome;
+        long cpf;
+        long matricula;
+
+        System.out.println("∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎");
+        System.out.println("∎∎∎∎∎∎∎∎∎∎∎∎∎ CADASTRO ALUNO ∎∎∎∎∎∎∎∎∎∎∎∎∎");
+        System.out.println("∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎");
+
+        System.out.println("");
+        System.out.println("Digite o nome do aluno: ");
+        nome = sc.nextLine();
+        System.out.println("Digite o CPF do aluno: ");
+        cpf = sc.nextLong();
+        System.out.println("Digite a matrícula do aluno");
+        matricula = sc.nextLong();
+
+        System.out.println("Tentando cadastrar aluno...");
+
+        Aluno aluno = new Aluno(matricula, nome, cpf);
+
+        try {
+            this.alunos.adicionaAluno(aluno);
+        } catch (AlunoJaCadastradoException ex) {
+            System.out.println("O aluno já está cadastrado e não foi cadastrado.");
+        } catch (IOException ex) {
+            System.out.println("Não foi possível salvar o aluno ao banco de dados. Poderá ocorrer perda de dados por isso.");
+        }
+
+        System.out.println("Fim!");
+        System.out.println("");
+
     }
 
 }
