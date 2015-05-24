0112e25d83f18b9c64321309fc973f9e2e9f6a4f
Fabiano Amaral Freitas <fabianin.amaral@gmail.com>
2015-05-24 05:38:48 -0300

    Modificado

---
+++ src/model/dao/DisciplinaDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dao;
+
+import exceptions.DisciplinaJaCadastradaException;
+import model.pojo.Disciplina;
+
+/**
+ *
+ * @author Fabiano
+ */
+public interface DisciplinaDao {
+    public void addDisciplina(Disciplina disciplina)throws DisciplinaJaCadastradaException;
+    public Disciplina getDisciplinaById(int id);
+    
+}
--- src/model/dao/txt/DisciplinaDaoTxt.java
+++ src/model/dao/txt/DisciplinaDaoTxt.java
@@ -5,12 +5,15 @@
  */
 package model.dao.txt;
 
+import exceptions.TurmaJaCadastradaException;
+import exceptions.DisciplinaJaCadastradaException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import model.dao.DisciplinaDao;
 import model.pojo.Disciplina;
 import model.pojo.Turma;
 import org.apache.commons.io.*;
@@ -22,7 +25,7 @@ import org.json.JSONObject;
  *
  * @author Fabiano
  */
-public class DisciplinaDaoTxt {
+public class DisciplinaDaoTxt implements DisciplinaDao {
 
     private final String filePath;
     private final File file;
@@ -35,13 +38,43 @@ public class DisciplinaDaoTxt {
         this.fileContent = FileUtils.readLines(this.file, "UTF-8");
     }
 
-    private boolean addDisciplina(Disciplina disc) {
-        if (this.disciplinas.contains(disc)) {
-            return false;
-        } else {
-            this.disciplinas.add(disc);
-            return true;
+    private void initDao() {
+        try {
+            List<String> conteudo = FileUtils.readLines(this.file, "UTF-8");
+            conteudo.stream().filter(str -> !str.isEmpty()).forEach((String str) -> {
+                try {
+                    JSONObject objDisciplina = new JSONObject(str);
+                    Disciplina disciplina;
+                    disciplina = DaoTxtUtils.createDisciplinaFromJSON(objDisciplina);
+                    this.disciplinas.add(disciplina);
+                } catch (JSONException ex) {
+                    Logger.getLogger(NotaDaoTxt.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (TurmaJaCadastradaException ex) {
+                    Logger.getLogger(DisciplinaDaoTxt.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            });
+        } catch (IOException e) {
+            System.out.println("Houve um erro: " + e.getMessage());
         }
     }
+
+    public ArrayList<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+    @Override
+    public void addDisciplina(Disciplina disciplina) throws DisciplinaJaCadastradaException{
+        if(this.disciplinas.contains(disciplina)){
+            throw new DisciplinaJaCadastradaException();
+        }else{
+            this.disciplinas.add(disciplina);
+        }
+    }
+
+    @Override
+    public Disciplina getDisciplinaById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 
 }
