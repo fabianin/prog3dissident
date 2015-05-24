979736691dfeccd837f1173b4b0b0c44c4d0fa19
Fabiano Amaral Freitas <fabianin.amaral@gmail.com>
2015-05-24 18:14:16 -0300

    Adicionado metodo cadastrar aluno.

--- src/view/console/AlunoView.java
+++ src/view/console/AlunoView.java
@@ -5,6 +5,11 @@
  */
 package view.console;
 
+import exceptions.AlunoJaCadastradoException;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import model.dao.AlunoDao;
 import model.dao.txt.AlunoDaoTxt;
 import model.pojo.Aluno;
@@ -19,6 +24,25 @@ public class AlunoView {
     }
     public static void imprimirAlunos(AlunoDao alunos){
         alunos.getAlunos().stream().forEach((x) -> System.out.println(x));
+    }
+    public static void cadastraAluno(AlunoDao alunos) throws IOException{
+        Aluno al;
+        Scanner sci = new Scanner(System.in);
+        String nome;
+        long cpf;
+        long matricula;
+        System.out.println("Digite o nome do aluno: ");
+        nome = sci.nextLine();
+        System.out.println("Digite o CPF do aluno: ");
+        cpf = sci.nextLong();
+        System.out.println("Digite a matricula do ALuno: ");
+        matricula = sci.nextInt();
+        al = new Aluno(matricula, nome, cpf);
+        try {
+            alunos.adicionaAluno(al);
+        } catch (AlunoJaCadastradoException ex) {
+            System.out.println("Esse aluno já foi cadastrado no sistema, por favor tente novamente");
+        }
     }
     
 }
